39a3b0bae16fdaea8c2e22a0760772013035be42
gnulitskaya <v.gnulitskaya@finflowsoft.com>
2022-07-10 21:32:38 +0300

    add quiz item

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/pages/root/root.module').then(m => m.RootModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
