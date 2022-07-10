785a9fd3b51e78ceb48b3353bac531efe413de7f
gnulitskaya <v.gnulitskaya@finflowsoft.com>
2022-07-10 13:49:08 +0300

    import useful material modules

--- src/app/modules/shared/app-shared.module.ts
+++ src/app/modules/shared/app-shared.module.ts
@@ -4,6 +4,19 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 // import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatDialogModule } from '@angular/material/dialog';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+// import { MatRadioModule } from '@angular/material/radio';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatSliderModule } from '@angular/material/slider';
+// import { MatSnackBarModule } from '@angular/material/snack-bar';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatTabsModule } from '@angular/material/tabs';
+// import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
   declarations: [],
